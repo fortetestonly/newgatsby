54d85b316fa7a3c6d68515cb3720919764a6359f
fortetestonly <43376028+fortetestonly@users.noreply.github.com>
2019-07-25 14:46:25 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+himport React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Welcome Forte</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
